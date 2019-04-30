81db3b6d526ff571a0bf9e8bfc14aae928eaa8c9
15701362@qq.com <15701362@qq.com>
2019-04-30 11:27:26 +0800

    2019年4月30日 孙亮 测试使用IDE 插件提交项目到 本地仓库

--- src/main/java/com/pb/t106/gitdemo/controller/GitDemo.java
+++ src/main/java/com/pb/t106/gitdemo/controller/GitDemo.java
@@ -1,4 +1,14 @@
 package com.pb.t106.gitdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GitDemo {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "demo";
+    }
+
 }
